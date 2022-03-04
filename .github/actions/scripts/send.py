ac063a77062d9de88764f41dd0d0e39845984604
Pablo Takara <pablo.takara@glovoapp.com>
2022-03-04 19:36:44 +0100

    Debug

--- .github/actions/scripts/send.py
+++ .github/actions/scripts/send.py
@@ -7,11 +7,13 @@ import json
 def parse_domain_yaml(domain_file: str) -> dict:
     with open(domain_file, 'r') as f:
         content = yaml.safe_load(f)
+        print(content)
     return content
 
 def parse_data_product_yaml(data_product_file: str) -> dict:
     with open(data_product_file, 'r') as f:
         content = yaml.safe_load(f)
+        print(content)
     return content
 
 def send(message_body: dict) -> None:
@@ -29,9 +31,14 @@ if __name__ == "__main__":
     message_body = {}
     data_products = []
     for changed_file in changed_files:
+        print(changed_file)
         if changed_file.split('/')[-1] == 'domain.yaml':
+            print("In domain")
             message_body['domain'] = parse_domain_yaml(changed_file)
         elif changed_file.split('/')[-1] == 'data_product.yaml':
+            print("In DP")
             data_products.append(parse_data_product_yaml(changed_file))
-    message_body['data_products'] = data_products
-    send(message_body)
+    if len(data_products) > 0:
+        message_body['data_products'] = data_products
+    if len(message_body) > 0:
+        send(message_body)
